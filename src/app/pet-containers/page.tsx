76868304ccd68ef49fd40b9178abf6d2d9b676c9
Viktor Varenik <yavarenikya@gmail.com>
2025-07-24 17:41:08 +0300

    chore: adjust pet containers grid

--- src/app/pet-containers/page.tsx
+++ src/app/pet-containers/page.tsx
@@ -82,7 +82,7 @@ export default function PetContainersPage() {
                     onChange={e => setSearchTerm(e.target.value)}
                 />
             </div>
-            <div className="w-full grid justify-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  auto-rows-fr gap-4">
+            <div className="w-full grid justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6  auto-rows-fr gap-4">
                 {filteredContainers.map((pet, index) => {
                     return <PetContainerItem key={index} petContainer={pet} />;
                 })}
